f7b477bde604d131618abbc505dba74c2b5ccd87
Osarobari Hope <mariushope007@gmail.com>
2022-11-22 04:44:48 -0800

    printf

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is for printf Alx collaboration project. it will replicate the C stabdard liberary printf() function.
---
+++ handle_print.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
