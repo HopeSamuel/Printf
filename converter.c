30781d57e290e40bbfebfc9c8d368f1263d0fcec
Osarobari Hope <mariushope007@gmail.com>
2022-12-08 01:41:43 -0800

    w

--- converter.c
+++ converter.c
@@ -7,22 +7,25 @@
   * @lowercase: flag if hexa values need to be lowercase
   * Return: result string
   */
- char *convert(unsigned long int num, int base, int lowercase)
-{
-	static char *rep;
-	static char buffer[50];
-	char *ptr;
 
-	rep = (lowercase)
-		? "0123456789abcdef"
-		: "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = '\0';
-	do 
-	{
-		*--ptr = rep[num % base];
-		num /= base;
-	}
-	while (num != 0);
-	return (ptr);
+char *convert(unsigned long int num, int base, int lowercase)
+{
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
+
+	rep = (lowercase)
+
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (ptr);
 }
